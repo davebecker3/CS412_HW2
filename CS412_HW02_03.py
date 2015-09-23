dbffd79d54b5d0ad706fdc58164bb68617ac05ce
davebecker3 <dave.becker@gmail.com>
2015-09-23 04:01:57 -0500

    Parsed datetime and found lo/high temps.

--- CS412_HW02_03.py
+++ CS412_HW02_03.py
@@ -1,5 +1,5 @@
 import csv
-
+from datetime import datetime, date, time
 
 print("    This is the output of file: \'CS412_HW02_03py\'.")
 
@@ -13,16 +13,37 @@ def read_print():
         for stuff in reader:
             # input_list.append(stuff[0])
             if(stuff[0] == 'Calumet Beach'):
-                input_list = []
+                in_lst = []
                 # input_list.clear()
-                input_list.append(stuff[9])
-                input_list.append(float(stuff[2]))
-                calumet.append(input_list)
+                in_lst.append(datetime.strptime(stuff[9], '%m/%d/%Y %H:%M %p'))
+                in_lst.append(float(stuff[2]))
+                calumet.append(in_lst)
                 print(calumet)
         print("    The length of the list \"calumet\" is: ", len(calumet))
-        print(input_list)
-        print("    The length of the input list is: ", len(input_list))
+        print(in_lst)
+        print("    The length of the input list is: ", len(in_lst))
 
+        max_temp = 0.0
+        max_temp_index = 0
+        for stuff in calumet:
+            if(stuff[1] > max_temp):
+                max_temp = stuff[1]
+                max_temp_index = calumet.index(stuff)
+                print("    The max temp found so far is: ", max_temp)
+        print("    The final max temp is: ", max_temp)
+        print("    The index of the max temp is: ", max_temp_index)
+        print("    The sub-list at that index is: ", calumet[max_temp_index])
+
+        min_temp = 100.0
+        min_temp_index = 0
+        for stuff in calumet:
+            if(stuff[1] < min_temp):
+                min_temp = stuff[1]
+                min_temp_index = calumet.index(stuff)
+                print("    The min temp found so far is: ", min_temp)
+        print("    The final min temp is: ", min_temp)
+        print("    The index of the min temp is: ", min_temp_index)
+        print("    The sub-list at that index is: ", calumet[min_temp_index])
 
 if __name__ == "__main__":
     import sys
